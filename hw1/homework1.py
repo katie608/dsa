be0e17b108acee2fc087fe42d4efc13a636f12cc
katie608 <katief650@gmail.com>
2020-01-27 17:42:19 -0500

    Add pytest tests to program

--- hw1/homework1.py
+++ hw1/homework1.py
@@ -39,6 +39,21 @@ def longest_increasing_sequence(list):
     return longest_seq
 
 
+# test using pytest test function
+import pytest
+def test_function():
+	assert longest_increasing_sequence([3, 1, 2, 3, 5, 8, 2]) == [1, 2, 3, 5, 8],"test failed"
+	assert longest_increasing_sequence([1, 2, 0, 4, 8, 9, 3]) == [0, 4, 8, 9],"test failed"
+	assert longest_increasing_sequence([]) == [],"test failed"
+	assert longest_increasing_sequence([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47],"test failed"
+	assert longest_increasing_sequence([4, 3, 0, 0]) == [0],"test failed"
+	assert longest_increasing_sequence([1, 2, 3, 4, 5, 6, 3, 4, 5]) == [1, 2, 3, 4, 5, 6],"test failed"
+
+
+
+
 if __name__ == '__main__':
+
+    # test again using doctest because I just learned pytest and don't trust it yet
     import doctest
     doctest.testmod()
