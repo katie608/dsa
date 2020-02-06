c224ee349a391ad64c443da7c4f815fd613d300a
katie608 <katief650@gmail.com>
2020-02-06 13:13:28 -0500

    Do more work on doubly linked list

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -6,28 +6,30 @@ class Node:
     """
     defines one node of the linked list
     """
-    def __init__(self, val, prev, next):
+    def __init__(self, val):
         """
         val: value of node
         next: pointer to next node of linked list
         """
         self.val = val
-        self.prev = prev
-        self.next = next
+        self.prev = None
+        self.next = None
 
 class DLL:
     """
     doubly linked list
     """
-    def __init__(self, head, tail, length):
+    def __init__(self):
         """
         head: first node in linked list
         tail: last node in linked list
         length: integer value of number of nodes in the list
         """
-        self.head = head
-        self.tail = tail
-        self.length = length
+        self.head = None
+        # head.next = self.index(1)
+        self.tail = None
+        # self.tail.prev = self.index(self.length() -1)
+        self.length = 0
     def length(self):
         """ returns length of linked list """
         return self.length
@@ -38,21 +40,33 @@ class DLL:
         """
         curr = self.head
         for j in range (i):
+            if curr == None:
+                return None
             curr = curr.next
         return curr
     def push(self, new):
         """ add a new node to the front of the list
         """
-        new.next = self.head
-        self.head = new
+        if self.length == 0:
+            self.head = new
+            self.tail = new
+        else:
+            new.next = self.head
+            self.head = new
+            self.head.prev = new
         self.length += 1 # keep track of list length
     def insert(self, new, i):
         """  insert a new node after a given node
         not sure if I accounted for all cases here
         eg: if end of list, set to tail, if beginning, set to head
         """
-        new.next = self.index(i)
-        self.index(i-1).next = new
+        new.prev = self.index(i)
+        if self.index(i).next is None:
+            self.tail = new
+        else:
+            new.next = self.index(i).next
+            new.next.prev = new
+            # self.index(i+1).prev = new <-- does same thing as above?
         self.length += 1 # keep track of list length
     def delete(self, i):
         """ deletes a node at a given index
@@ -71,22 +85,27 @@ class DLL:
         return sum
 
 
-a = Node(10, None, None)
-b = Node(11, None, None)
+a = Node(10)
+b = Node(11)
 
 # d = Node(13, None)
 # c = Node(12, None)
 # b = Node(11, c)
 
 
-llone = DLL(a, b, 2)
+llone = DLL()
 
 # llone.insert(b, 1)
-# llone.push(d)
+llone.push(b)
+llone.push(a)
 
 print("length: ",llone.length)
 print("head value: ", llone.head.val)
+print("head.prev: ", llone.head.prev)
+print("head.next: ", llone.head.next)
 print("tail value: ", llone.tail.val)
+print("tail.prev: ", llone.tail.prev)
+print("tail.next: ", llone.tail.next)
 
 # for i in range(llone.length):
     # print("List element ", i, " is ", llone.index(i).val)
