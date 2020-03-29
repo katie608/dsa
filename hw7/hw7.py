055da9ed3845e36a19e32ae97ff97a17fdc20f60
katie608 <katief650@gmail.com>
2020-03-29 17:08:23 -0400

    Finish HW7

--- hw7/hw7.py
+++ hw7/hw7.py
@@ -2,7 +2,7 @@
 Homework 7: Graphs
 """
 
-""" Part 1 """
+""" Part 2 """
 import networkx as nx
 
 def find_components(G):
@@ -47,4 +47,52 @@ test_function()
 
 
 
-""" Part 2 """
+""" Part 3 """
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_random_binomial_graph(num_nodes, p):
+    G = nx.Graph()
+    for i in range(num_nodes):
+        G.add_node(i)
+    for i in range(num_nodes):
+        for j in range(num_nodes):
+            if np.random.randint(0, 1/p) == 1: # make a choice
+            # basically roll a p sided die, and add edge if number is 1
+                G.add_edge(i, j)
+    return G
+
+
+def generate_random_graphs():
+    x = []
+    y = []
+    for i in range(45):
+        for j in range(1, 101):
+            k = 0
+            num_components = 1
+            while num_components == 1:
+                # exit loop if any graph has more than 1 component
+                G = generate_random_binomial_graph(i+5, j/100)
+                num_components = find_components(G)
+                k+=1
+                # count number of times loop has run, if loop has run all 10 times
+                # that means that all 10 graphs have 1 component,
+                # so add that entry to graph lists
+                if k == 10:
+                    x.append(i) # i is num_nodes (5-50)
+                    y.append(j/100) # j is probability (1-100)
+                    break
+    return x, y
+
+
+def graph_smallest_prob():
+    x, y = generate_random_graphs()
+    plot = plt.scatter(x, y)
+
+
+    plt.show()
+
+graph_smallest_prob()
+# g = generate_random_binomial_graph(4, 0.4)
+# print(g.nodes(), g.edges())
