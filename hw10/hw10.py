3a4718485b3a63f03e4b99ad57a3ed249ab9d9a8
katie608 <katief650@gmail.com>
2020-04-23 20:29:57 -0400

    Do more work on homework 10

--- hw10/hw10.py
+++ hw10/hw10.py
@@ -33,15 +33,15 @@ def read_tsp(filename):
                     j += 1
     return C, n
 
-C, n = read_tsp('gr17.tsp')
-# print(C)
+C, n = read_tsp("gr17.tsp")
 
 def get_distance(C, path):
     """ Returns the sum of all the edge lengths in a certain path
     """
     dist = 0
-    for i in range(len(path)):
-        dist += C[i, path[i]]
+    for i in range(len(path)-1):
+        dist += C[path[i], path[i+1]]
+    dist += C[path[len(path)-1], path[0]] 
     return dist
 
 
@@ -56,7 +56,7 @@ def greedyHeuristic(C, num_nodes):
     path = []
     total_dist = 0
     for i in range(num_nodes):
-        dist = 9999999 # set initial distance to "infinity"
+        dist = float('inf') # set initial distance to infinity
         nearest = None
         for j in range(num_nodes): # add the index of the min of C[i, :]
             if C[i, j] < dist and i != j and j not in path:
@@ -64,11 +64,12 @@ def greedyHeuristic(C, num_nodes):
                 nearest = j
         path.append(nearest)
         total_dist += dist
-    return path, total_dist
+    return path, get_distance(C, path)
 
-path, total_dist = greedyHeuristic(C, n)
-print(path, total_dist)
-# print(get_distance(path, C))
+path, dist = greedyHeuristic(C, n)
+print(path, dist)
+# print(len(set(path)) == len(path))
+# print(len(path) == n)
 
 def localSearch(C, curr_path):
     """ Starts with potential solution and looks for improvments until it can't
@@ -76,27 +77,88 @@ def localSearch(C, curr_path):
     curr_path is a python list of nodes, representing a path
     2-opt: removes 2 edges then reconnects optimally
     """
+
     num_nodes = len(curr_path)
-    max_iterations = 20
+    max_iterations = 100
     curr_dist = get_distance(C, curr_path)
     for i in range(max_iterations):
         # if no changes are made, break loop
         for j in range(num_nodes):
-            for k in range(num_nodes):
+            for k in range(j, num_nodes): # might need j+1
                 # swap two edges
-                new_path = curr_path
+                new_path = curr_path[:]
                 new_path[k-1] = curr_path[k]
                 new_path[k] = curr_path[k-1]
                 new_dist = get_distance(C, new_path)
-                # print(new_dist)
-                if new_dist < curr_dist:
+                # check to make sure it is not added twice?
+                if new_dist < curr_dist and len(set(new_path)) ==  len(new_path):
                     curr_path = new_path
                     curr_dist = new_dist
-    return curr_path, curr_dist
+    return curr_path, int(curr_dist)
 
-print(localSearch(C, path))
+
+
 
 def test():
     """ Tests runtime and optimality gaps of greedy heuristic and local search
+
     """
-    pass
+    optimal_distances = {
+        "gr17": 2085,
+        "gr21": 2707,
+        "gr24": 1272,
+        "gr48": 5046
+    }
+
+    filenames = ['gr17.tsp', 'gr21.tsp', 'gr24.tsp', 'gr48.tsp']
+    for file in filenames:
+        print(file, ":")
+        C, n = read_tsp(file)
+        optimal_dist = optimal_distances[file[0:4]]
+        path, heuristic_dist = greedyHeuristic(C, n)
+        print("Distance from Greedy Heuristic:", heuristic_dist)
+        print("Greedy Heuristic optimality ratio:", (heuristic_dist-optimal_dist)/optimal_dist)
+        local_dist = localSearch(C, path)[1]
+        print("Distance from Local Search:", local_dist)
+        print("Local Search optimality ratio:", (local_dist-optimal_dist)/optimal_dist)
+        print("Optimal Distance:", optimal_dist)
+        print("")
+
+test()
+
+"""
+Results from test:
+
+gr17.tsp :
+Distance from Greedy Heuristic: 4865.0
+Greedy Heuristic optimality ratio: 1.3333333333333333
+Distance from Local Search: 4220
+Local Search optimality ratio: 1.023980815347722
+Optimal Distance: 2085
+
+gr21.tsp :
+Distance from Greedy Heuristic: 7320.0
+Greedy Heuristic optimality ratio: 1.7041004802364241
+Distance from Local Search: 5422
+Local Search optimality ratio: 1.0029553010712966
+Optimal Distance: 2707
+
+gr24.tsp :
+Distance from Greedy Heuristic: 3446.0
+Greedy Heuristic optimality ratio: 1.7091194968553458
+Distance from Local Search: 2784
+Local Search optimality ratio: 1.1886792452830188
+Optimal Distance: 1272
+
+gr48.tsp :
+Distance from Greedy Heuristic: 19273.0
+Greedy Heuristic optimality ratio: 2.8194609591755846
+Distance from Local Search: 15128
+Local Search optimality ratio: 1.9980182322631788
+Optimal Distance: 5046
+
+It seems like my greedy heurisitc algorithm does a decent job with smaller
+datasets, but does a really bad job with big sets. Also the local search
+helps optimize the algorithm in every instance, which is good.
+
+"""
